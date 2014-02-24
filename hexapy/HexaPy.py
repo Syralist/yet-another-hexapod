5ab36c48f5c64f14b9c2a5a9142a7bbe6f472d95
thomas <thomas@thomas-Linux>
2014-02-24 19:55:28 +0100

    Added empty eclipse projects for Python and Android.
    Signed-off-by: thomas <thomas@thomas-Linux>

---
+++ hexapy/HexaPy.py
@@ -0,0 +1,17 @@
+'''
+Created on 24.02.2014
+
+@author: thomas
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
