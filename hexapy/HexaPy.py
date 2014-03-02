056d028ecd056413081c62430d193dca13caae8a
thomas <thomas@thomas-Linux>
2014-03-02 18:01:37 +0100

    Test for TCP Server.
    Signed-off-by: thomas <thomas@thomas-Linux>

--- hexapy/HexaPy.py
+++ hexapy/HexaPy.py
@@ -4,6 +4,8 @@ Created on 24.02.2014
 @author: thomas
 '''
 
+import socket
+
 class MyClass(object):
     '''
     classdocs
@@ -14,4 +16,23 @@ class MyClass(object):
         '''
         Constructor
         '''
-        
+        pass
+    
+if __name__ == "__main__":
+    TCP_IP = '127.0.0.1'
+    TCP_PORT = 55555
+    BUFFER_SIZE = 20
+    MESSAGE = "Hello, World!"
+    
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((TCP_IP, TCP_PORT))
+    s.listen(1)
+    
+    conn, addr = s.accept()
+    print 'Connection address:', addr
+    while 1:
+        data = conn.recv(BUFFER_SIZE)
+        if not data: break
+        print "received data:", data
+        conn.send(data)  # echo
+    conn.close()
