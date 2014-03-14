f39a9b565fa2ce39e3fb98310951f0e41c42517c
Thomas Pi <thomas.helmke@gmx.de>
2014-03-14 17:34:16 +0000

    Fixed time display. Now only once a second.

--- hexapy/LCDClasses.py
+++ hexapy/LCDClasses.py
@@ -25,6 +25,7 @@ class LcdController(threading.Thread):
         self.running = True
         
         self.menu = Menu(self)
+        self.menu.start()
         
     def run(self):
         while self.running:
@@ -32,6 +33,7 @@ class LcdController(threading.Thread):
     
     def Exit(self):
         self.LCD.backlight(self.LCD.OFF)
+        self.LCD.clear()
         self.menu.Exit()
         self.menu.join()
         self.running = False
@@ -48,6 +50,7 @@ class LcdController(threading.Thread):
     
 class Menu(threading.Thread):
     def __init__(self, parent):
+        threading.Thread.__init__(self)
         self.mainmenulevel = 0
         self.submenu1level = 0
         
@@ -60,11 +63,19 @@ class Menu(threading.Thread):
         self.parent.push(self.mainmenu[self.mainmenuitem])
         
         self.running = True
+        self.timestamp = int(time.time())
+        self.timechanged = False
         
     def run(self):
         while self.running:
+            if int(time.time()) > self.timestamp:
+                self.timestamp = int(time.time())
+                self.timechanged = True
+            else:
+                self.timechanged = False
             if self.mainmenulevel == 0 and self.mainmenuitem == 0:
-                self.parent.push2(self.mainmenu[self.mainmenuitem],time.strftime("%H:%m:%s"))
+                if self.timechanged:
+                    self.parent.push2(self.mainmenu[self.mainmenuitem],time.strftime("%H:%M:%S"))
         
     def Exit(self):
         self.running = False
