fd075fe2235cc9f82731550ef662bf2c6a3d1e82
Thomas Helmke <thomas.helmke@gmx.de>
2014-06-07 16:52:25 +0200

    New class MoveJoint. Testing update cycle.
    Signed-off-by: Thomas Helmke <thomas.helmke@gmx.de>

--- hexapy/ControlClasses.py
+++ hexapy/ControlClasses.py
@@ -3,6 +3,8 @@ Created on 07.06.2014
 
 @author: Thomas
 '''
+import threading
+import time, sleep
 
 class MessageHandler(object):
     '''
@@ -15,6 +17,8 @@ class MessageHandler(object):
         Constructor
         '''
         self.ServoHandler = ServoHandler
+        self.Mover = MoveJoint()
+        self.Mover.start()
     
     def push(self, data):
         self.DirectPush = False
@@ -26,6 +30,7 @@ class MessageHandler(object):
             self.Joint = ""
             self.Angle = 0.0
             self.Message = ""
+            self.Dummy = False
             for Message in self.Messages:
                 self.Parts = Message.split()
                 if self.Parts[0] == "setJoint":
@@ -38,7 +43,34 @@ class MessageHandler(object):
                             pass
                         self.Angle = 0.0
                         self.Joint = ""
+                elif self.Parts[0] == "moveJoint":
+                    self.Dummy = not self.Dummy
+                    self.Mover.SetUpdate(self.Dummy)
             pass
     
     def Exit(self):
-        pass
+        self.Mover.Exit()
+        self.Mover.join()
+    
+class MoveJoint(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.running = True
+        self.doUpdate = False
+    
+    def run(self):
+        while self.running:
+            if self.doUpdate:
+                self.startTime = time()
+                self.UpdateJoints()
+                self.endTime = time()-self.startTime
+                sleep(1.0-self.endTime)
+        
+    def Exit(self):
+        self.running = False
+        
+    def SetUpdate(self, doUpdate):
+        self.doUpdate = doUpdate
+        
+    def UpdateJoints(self):
+        print self.startTime
