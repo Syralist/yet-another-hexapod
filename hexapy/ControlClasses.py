92f3c2fb78ea23af47fd1c905cbf0ce1425aab5c
Thomas Helmke <thomas.helmke@gmx.de>
2014-06-07 15:25:32 +0200

    New Message structure.
    Signed-off-by: Thomas Helmke <thomas.helmke@gmx.de>

--- hexapy/ControlClasses.py
+++ hexapy/ControlClasses.py
@@ -21,21 +21,23 @@ class MessageHandler(object):
         if self.DirectPush:
             self.ServoHandler.push(data)
         else:
-            self.MessageParts = data.split()
+            self.Messages = data.split(';')
             self.Servos = self.ServoHandler.getJoints()
             self.Joint = ""
             self.Angle = 0.0
-            for Part in self.MessageParts:
-                if Part in self.Servos:
-                    self.Joint = Part
-                elif self.Joint != "":
-                    self.Angle = float(Part)
-                    try:
-                        self.ServoHandler.setAngle(self.Joint, self.Angle)
-                    except:
-                        pass
-                    self.Angle = 0.0
-                    self.Joint = ""
+            self.Message = ""
+            for Message in self.Messages:
+                self.Parts = Message.split()
+                if self.Parts[0] = "setJoint":
+                    if self.Parts[1] in self.Servos:
+                        try:
+                            self.Joint = self.Parts[1]
+                            self.Angle = float(self.Parts[2])
+                            self.ServoHandler.setAngle(self.Joint, self.Angle)
+                        except:
+                            pass
+                        self.Angle = 0.0
+                        self.Joint = ""
             pass
     
     def Exit(self):
